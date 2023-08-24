37668df9eb3ce81b73ab58fadfb395ccfecfbd39
Eduardo Almeida <eduardo.almeida.job@gmail.com>
2023-08-24 16:36:28 -0300

    test: todoService create

--- mocking-with-node-test-runner-template/test/todoService.test.js
+++ mocking-with-node-test-runner-template/test/todoService.test.js
@@ -1,6 +1,8 @@
-import { describe, it, beforeEach } from "node:test";
-import TodoService from "../src/todoService.js";
+import { describe, it, beforeEach, before, after } from "node:test";
 import assert from "node:assert";
+import crypto from "node:crypto";
+import TodoService from "../src/todoService.js";
+import Todo from "../src/todo.js";
 
 describe("todoService test Suite", () => {
   describe("#list", () => {
@@ -39,6 +41,22 @@ describe("todoService test Suite", () => {
   describe("#create", () => {
     let _todoService;
     let _dependencies;
+    const mockCreateResult = [
+      {
+        text: "I must plan my trip to Europe",
+        when: new Date("2021-03 - 22T00:00:00.000Z"),
+        status: "late",
+        id: "aaf5de1d-0878-4022-8ab0-fbe57c45e78e",
+      },
+    ];
+
+    const DEFAULT_ID = "0001";
+    before(() => {
+      crypto.randomUUID = () => DEFAULT_ID;
+    });
+    after(async () => {
+      crypto.randomUUID = (await import("node:crypto")).randomUUID;
+    });
 
     beforeEach((context) => {
       _dependencies = {
@@ -47,6 +65,27 @@ describe("todoService test Suite", () => {
         },
       };
       _todoService = new TodoService(_dependencies);
+    });
+
+    it(`shouldn't save todo item with invalid data`, async () => {
+      const input = new Todo({
+        text: "",
+        when: "",
+      });
+      const expected = {
+        error: {
+          message: "invalid data",
+          data: {
+            text: "",
+            when: "",
+            status: "",
+            id: DEFAULT_ID,
+          },
+        },
+      };
+
+      const result = await _todoService.create(input);
+      assert.deepStrictEqual(JSON.stringify(result), JSON.stringify(expected));
     });
   });
 });
